ea9341ab86a3b6a21e1e64b645537822ea6c812b
Jesse Hammond <jessethammond@gmail.com>
2017-11-13 17:37:07 -0500

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   mount Attachinary::Engine => "/attachinary"
+
+  resources :plants, only: [:index, :show]
+  resources :orders, only: [:show] # Order-items are within the order, does not need own path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
