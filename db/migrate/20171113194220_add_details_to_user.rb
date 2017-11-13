b395257f37c4fdc718d66db4bc7758d7e7e884a4
Jesse Hammond <jessethammond@gmail.com>
2017-11-13 14:47:00 -0500

    Add fields to the user model

---
+++ db/migrate/20171113194220_add_details_to_user.rb
@@ -0,0 +1,11 @@
+class AddDetailsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :street_address, :string
+    add_column :users, :city, :string
+    add_column :users, :country, :string
+    add_column :users, :postal_code, :string
+    add_column :users, :phone_number, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113184232) do
+ActiveRecord::Schema.define(version: 20171113194220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 20171113184232) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "street_address"
+    t.string "city"
+    t.string "country"
+    t.string "postal_code"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
