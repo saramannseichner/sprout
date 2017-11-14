777d75de88ad9866f78c31346335882e4ad23fab
Sara Mannseichner <saramannseichner@gmail.com>
2017-11-14 16:02:41 -0500

    sign up works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   helper_method :current_order
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def current_order
     if current_user.orders.any?
@@ -9,5 +11,10 @@ class ApplicationController < ActionController::Base
     else
       Order.create! user: current_user
     end
+  end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
