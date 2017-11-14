c2efeff8bf47f405f4ecb2084b45cadc45fc9793
Sara Mannseichner <saramannseichner@gmail.com>
2017-11-14 13:40:13 -0500

    address controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,19 @@
 class AddressesController < ApplicationController
+
+  def create
+    @address = Address.new(address_params)
+    @user = current_user
+    @address.user_id = @user.id
+
+  end
+
+  private
+
+  def set_address
+     @address = Address.find(params[:id])
+  end
+
+  def address_params
+    params.require(:address).permit(:street_address, :city, :country, :postal_code, :phone_number)
+  end
 end
