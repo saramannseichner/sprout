045211bfcb78f59377b2911352370fc1d9a5a6a2
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-20 10:38:47 -0500

    renamed fb id environment variable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,7 +275,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   Devise.setup do |config|
-  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
+  config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_SECRET"],
     scope: 'email',
     info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
