c3ecb6aac4e76b80a5a1db87241063e69f19c3e2
Jesse Hammond <jessethammond@gmail.com>
2017-11-21 10:59:15 -0500

    Function works for fade in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,3 @@
 //= require_tree .
 
 
-$(document).ready(function(){
-    $(".fadeIn").show(0).delay(500).fadeIn(3000)
-});
-
-$(document).ready(function(){
-    $(".fadeOut").hide(0).delay(500).fadeIn(3000)
-});
