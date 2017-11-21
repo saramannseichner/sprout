c6f5de5521ea57430e999364b284f78f7a21760a
Jesse Hammond <jessethammond@gmail.com>
2017-11-21 10:39:01 -0500

    Add js classes for fadeIn and fadeOut

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,12 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).ready(function(){
+    $(".fadeIn").show(0).delay(500).fadeIn(3000)
+});
+
+$(document).ready(function(){
+    $(".fadeOut").hide(0).delay(500).fadeIn(3000)
+});
