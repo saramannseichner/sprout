219508f0cccaa34fd7055e216bae63a48fd2b34f
Sara Mannseichner <saramannseichner@gmail.com>
2017-11-21 12:25:39 -0500

    address is stored, alerts made

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,10 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).ready(function() {
+  setTimeout(function() {
+    $('.alert').slideUp()
+  }, 5000)
+})
--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,11 +1,49 @@
 class AddressesController < ApplicationController
+  #before_action :find_order_address, only: [:create]
+
   def create
-    @address = Address.new(address_params)
-    @address.user = current_user
-    @address.save
+
+    # if @address
+    #   puts 'updapte'
+
+    #   if @address.update(address_params)
+    #     respond_to do |format|
+    #       format.html { redirect_to new_order_path }
+    #       format.js {  flash[:notice] = "Saved !" } # <-- will render `app/views/reviews/create.js.erb`
+    #     end
+    #   else
+    #     respond_to do |format|
+    #       format.html { redirect_to new_order_path }
+    #       format.js  {  flash[:notice] = "Not saved !" }  # <-- will render `app/views/reviews/create.js.erb`
+    #     end
+    #   end
+    # else
+      puts 'create'
+      @address = Address.new(address_params)
+      @address.user = current_user
+
+
+
+      if @address.save
+        @alert_message = "Great Job"
+        @alert_message_success = true
+      else
+        @alert_message = "You Suck"
+        @alert_message_success = false
+      end
+
+      respond_to do |format|
+        format.js
+        format.html
+      end
+
   end
 
   private
+
+  def find_order_address
+    @address = current_order.address
+  end
 
   def address_params
     params.require(:address).permit(:street_address, :city, :country, :postal_code, :phone_number, :first_name, :last_name)
