b979bd27471bbd1f1e6b238aaa359365f850e6ce
Jesse Hammond <jessethammond@gmail.com>
2017-11-21 11:36:27 -0500

    added turbolinks, turbolinks transitions, cloudinary and attachinary to the application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,9 @@
 //= require jquery
 //= require jquery_ujs
+//= require cloudinary
+//= require attachinary
+//= require turbolinks
+//= require turbolinks_transitions
 //= require bootstrap-sprockets
 //= require_tree .
 
