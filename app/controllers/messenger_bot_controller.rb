9ac0aaa14cb665bf80b2854aa05a6690fe537521
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-23 13:07:38 -0500

    creating chatbot logic

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,35 +1,61 @@
-class MessengerBotController < ActionController::Base
-  #skip_before_action :authenticate_user!
-  #skip_before_action :verify_authenticity_token
+class MessengerBotController < ApplicationController
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
 
   def message(event, sender)
    # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
     # sender.reply({ text: "Reply: #{event['message']['text']}" })
     msg = event["message"]["text"]
-    sender_id = event["sender"]["id"].to_i
-    user = find_or_create_user(sender_id)
     # plants = find_user_plants(user)
-    quickreply = QuickReplyTemplate.new("Would you like us to remind you when you water your plant(s)?")
+    # quickreply = QuickReplyTemplate.new("Hi #{user.first_name}, would you like us to remind you when you water your plant(s)?")
 
-    quickreply.add_postback('Yes', 'callback_yes')
-    quickreply.add_postback('No', 'callback_no')
+    # quickreply.add_postback('Yes', 'callback_yes')
+    # quickreply.add_postback('No', 'callback_no')
 
-    sender.reply(quickreply.get_message)
+    # sender.reply(quickreply.get_message)
+    sender.reply({text: "♥️ from sprout"})
   end
 
   def optin(event, sender)
-    # trouve moi ou cree moi le user avec sender id
-     message_json = {
-        "text": "♥️ from sprout"
-        }
-      SendRequest.send(message_json,sender.sender_id)
+    user_id = event["optin"]["ref"].to_i
+    user = find_or_create_user(sender.sender_id, user_id)
+    plants = find_user_plants(user)
+
+    if plants.size == 1
+      quickreply = QuickReplyTemplate.new("Hi #{user.first_name}, would you like us to remind you when to water your new plant?")
+      quickreply.add_postback('Yes', 'callback_yes')
+      quickreply.add_postback('No', 'callback_no')
+    else
+      quickreply = QuickReplyTemplate.new("Hi #{user.first_name}, would you like us to remind you when to water your new plants?")
+      quickreply.add_postback('Yes', 'callback_yes')
+      quickreply.add_postback('No', 'callback_no')
+    end
+
+    # if plants.size == 1
+    #  message_json = {
+    #     "text": "Hi #{user.first_name}, let's set up some watering reminders for the plant you bought today."
+    #     }
+    #   SendRequest.send(message_json,sender.sender_id)
+    # else
+    #   message_json = {
+    #     "text": "Hi #{user.first_name}, let's set up some watering reminders for the #{plants.size} plants you bought today."
+    #     }
+    #   SendRequest.send(message_json,sender.sender_id)
+    # end
+    message_json = quickreply.get_message
+    SendRequest.send(message_json,sender.sender_id)
   end
 
   def postback(event, sender)
+    user = find_or_create_user(sender.sender_id)
+    plants = find_user_plants(user)
+
     payload = event["postback"]["payload"]
 
     if payload == 'callback_yes'
-      sender.reply({text: "Awesome, we'll send you water reminders!"})
+      if plants.size == 1
+        sender.reply({text: "Awesome, we'll send you water reminders #{plants.first.show_water_level.downcase}"})
+      end
     elsif payload == 'callback_no'
       sender.reply({text: "Ok, maybe next time :)"})
     end
@@ -37,12 +63,14 @@ class MessengerBotController < ActionController::Base
 
   private
 
-  def find_or_create_user(sender_id)
+  def find_or_create_user(sender_id, user_id = nil)
     if User.find_by(facebook_id: sender_id)
       User.find_by(facebook_id: sender_id)
     else
-      current_user.facebook_id = sender_id
-      current_user.save
+      user = User.find(user_id)
+      user.facebook_id = sender_id
+      user.save
+      user
     end
   end
 
