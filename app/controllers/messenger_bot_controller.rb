cc77ed222ead292a1c41425bf64fdb565e5ea0e2
Stephanie Savoie <stephsavoie4@gmail.com>
2017-11-22 22:05:59 -0500

    started config of messenger checkbox

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -6,7 +6,13 @@ class MessengerBotController < ActionController::Base
    # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
     # sender.reply({ text: "Reply: #{event['message']['text']}" })
     msg = event["message"]["text"]
-    quickreply = QuickReplyTemplate.new("Would you like us to remind you when you water your plant?")
+    # sender_id = event["sender"]["id"].to_i
+    # first_name = sender.get_profile[:body]["first_name"]
+    # last_name = sender.get_profile[:body]["last_name"]
+    # binding.pry
+    # user = find_or_create_user(first_name, last_name, sender_id)
+    # plants = find_user_plants(user)
+    quickreply = QuickReplyTemplate.new("Would you like us to remind you when you water your plant(s)?")
 
     quickreply.add_postback('Yes', 'callback_yes')
     quickreply.add_postback('No', 'callback_no')
@@ -17,9 +23,26 @@ class MessengerBotController < ActionController::Base
 
   def postback(event, sender)
     payload = event["postback"]["payload"]
-    case payload
-    when :something
-    #ex: process sender.reply({text: "button click event!"})
+
+    if payload == 'callback_yes'
+      sender.reply({text: "Great, let's nickname your new friend?"})
+    elsif payload == 'callback_no'
+      sender.reply({text: "Ok, maybe next time :)"})
     end
+  end
+
+  private
+
+  def find_or_create_user(first_name, last_name, sender_id)
+    if User.find_by(facebook_id: sender_id)
+      User.find_by(facebook_id: sender_id)
+    else
+      User.create(email: "#{sender_id}@fakeemail.com",first_name: first_name, last_name: last_name,password: sender_id,facebook_id: sender_id)
+    end
+  end
+
+  def find_user_plants(user)
+    last_order = user.orders.last
+    last_order.plants
   end
 end
